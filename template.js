3512d07a02885eb7338ff320f25f8579a4381471
markus-leben <61570879+markus-leben@users.noreply.github.com>
2022-06-23 22:20:47 -0600

    added modular search terms to template.js

--- template.js
+++ template.js
@@ -11,9 +11,41 @@
 
 (function() {
     'use strict';
-    var payload = window.location.href.replace("yoururl.here?tmpayload=", "").replace("+"," "); //format payload
-    //add additional text formating for payload as needed
-    document.getElementById("Paste Search Bar ID Here ").value = payload; //you can use other ways of finding elements too
-    document.getElementById("Paste Click Button ID Here").click(); //you're going to have to click something to send it
+    
+    //search term id dict, TODO: update to allow for different element finding methods or different interaciton.
+    //maintext is the default variable name of a plain search. 
+    //example_other_var is used for advanced search features
+    //for example, if you had a field labeled "artist name"
+    const search_term_id_dict = {
+        "maintext":"Paste Search Bar ID Here",
+        "example_other_var":"Paste ID of other search field here",
+        };
+    
+    let searchdict = {};
+    for (const i of payload){ //not sure how loops affect scope, not looking to mess around right now
+        var keyval = i.split(":");
+        if (keyval.length == 1){
+            keyval = ["maintext",keyval[0]];
+        }
+        if (!(keyval[0] in searchdict)){
+            searchdict[keyval[0]] = keyval[1];
+        }
+        else{
+            searchdict[keyval[0]] = searchdict[keyval[0]] + " " + keyval[1];
+        }
+    }
+    
+    //if you have to do any preprep before sending your payload, it goes here
+   
+
+    //this is the loop for putting search terms into their fields. 
+    for (const key in search_term_id_dict){
+        if (key in searchdict){
+            document.getElementById(search_term_id_dict[key]).value = searchdict[key];
+        }
+    }
+    
+    //click search button
+    document.getElementById("btnSearch").click(); //you may need to edit if their element isn't Id'd btnSearch, but btnSearch is a common ID
     //alternatively, you can send an enter keypress, but clicking is usually easier
 })();
